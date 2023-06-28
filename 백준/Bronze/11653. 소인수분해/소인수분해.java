727f019072cb646e9a645bf29db3134f32e44324
silversmell <64191166+silversmell@users.noreply.github.com>
2023-06-28 14:09:54 +0900

    [Bronze I] Title: 소인수분해, Time: 124 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,14 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        for(int i = 2; i <= N; i++) {
-            while(N % i == 0) {/* i=2 로 나누어서 안나누어지면 밖으로 나간다. i=3을 넣어본다 안나누어지면 밖으로 나간다. */
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            while (N % i == 0) {
                 System.out.println(i);
-                N = N/i;
+                N /= i;
             }
         }
+
+        if (N != 1)
+            System.out.println(N);
     }
 }
