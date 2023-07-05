f70427ca240c0300c46ce0338a94d350c55a841d
silversmell <64191166+silversmell@users.noreply.github.com>
2023-07-05 13:28:28 +0900

    [Bronze III] Title: 배수와 약수, Time: 200 ms, Memory: 17244 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a==0&&b==0)
+                break;
+            if (b % a == 0)
+                System.out.println("factor");
+            if (a % b == 0)
+                System.out.println("multiple");
+            if(b % a != 0 && a % b !=0)
+                System.out.println("neither");
+
+        }
+
+    }
+}
