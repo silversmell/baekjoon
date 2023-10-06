fe787dd362399b0ae6083491bd767d21179301b6
silversmell <64191166+silversmell@users.noreply.github.com>
2023-10-06 23:51:12 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+let a=+input[0];
+let b=+input[1];
+if(a<b){
+    console.log("<");
+}else if(a>b){
+    console.log(">");
+}
+else{
+    console.log("==");
+}
