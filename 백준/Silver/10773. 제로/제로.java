5e09a74f1566032039048209ff2b396155591e4c
silversmell <64191166+silversmell@users.noreply.github.com>
2024-01-04 19:07:39 +0900

    [Silver IV] Title: 제로, Time: 236 ms, Memory: 25256 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -7,23 +7,26 @@ import java.util.LinkedList;
 public class Main {
     static LinkedList<Integer> stack = new LinkedList<>(); //연결리스트
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); //명령어의 수
-        while(N -->0){
-            String command=br.readLine();
-            if(command.equals("0")){ //st 다음 토큰이 0 이면
-                stack.pop();
-            }
-            else{
-                stack.push(Integer.valueOf(command));
-            }
-        }
         int sum = 0;
-        for(int o : stack) {
-            sum += o;
-        }
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     int N= Integer.parseInt(br.readLine());
+     while(N-->0){
+         String command = br.readLine();
+         if(command.equals("0")){
+             stack.pop();
+         }
+         else{
+             stack.push(Integer.valueOf(command));
+         }
+     }
+     for(int o : stack){
+         sum+= o;
+     }
         System.out.println(sum);
+
 
     }
 
 }
+
+
